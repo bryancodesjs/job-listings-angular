422bcfb611b5e7ff23c4eeee69bc1854f72a0480
bryancodesjs <72945681+bryancodesjs@users.noreply.github.com>
2022-05-17 06:30:39 -0400

    added cookie policy logic

--- src/app/views/shared/cookies/cookies.component.ts
+++ src/app/views/shared/cookies/cookies.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cookies.component.scss']
 })
 export class CookiesComponent implements OnInit {
-
+  showCookieForm = false;
   constructor() { }
 
   ngOnInit(): void {
+    this.checkCookies();
   }
-
+  checkCookies(){
+    const consent = localStorage.getItem('magnetCookieConsent');
+    console.log(consent);
+    if(consent != null){
+      this.showCookieForm = false;
+    } else {
+      this.showCookieForm = true;
+    }
+  }
+  approveCookies(){
+    localStorage.setItem('magnetCookieConsent', 'true');
+    this.checkCookies();
+  }
 }
